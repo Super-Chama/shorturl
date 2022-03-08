a92e0c5085a0535586ba73b0b5d102a90610ce0b
Chamara Abesinghe <chamara@orangehrmlive.com>
2022-03-09 02:41:11 +0530

    loading state added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
   const [shortenUrl, setShortenUrl] = useState(null);
 
   const submitForm = async (event) => {
     event.preventDefault();
+    setLoading(true);
 
     const res = await fetch("/api/route", {
       body: JSON.stringify({
@@ -18,30 +21,37 @@ export default function Home() {
 
     const result = await res.json();
     setShortenUrl(`http://${result.host}/${result.hash}`);
+    setLoading(false);
   };
 
   return (
     <div className="App">
       {!shortenUrl ? (
-        <form onSubmit={submitForm}>
-          <h1>Short It!</h1>
-          <p>Enter your URL to get shortned...</p>
+        !loading ? (
+          <form onSubmit={submitForm}>
+            <h1>Short It!</h1>
+            <p>Enter your URL to get shortned...</p>
 
-          <div className="input-wrapper">
-            <input
-              id="url"
-              name="url"
-              type="text"
-              pattern="https?://.+"
-              placeholder="https://example.com"
-              required
-            />
-          </div>
+            <div className="input-wrapper">
+              <input
+                id="url"
+                name="url"
+                type="text"
+                pattern="https?://.+"
+                placeholder="https://example.com"
+                required
+              />
+            </div>
 
-          <div>
-            <button type="submit">Short It!</button>
+            <div>
+              <button type="submit">Short It!</button>
+            </div>
+          </form>
+        ) : (
+          <div className="card --flex">
+            <Image src="/loader.svg" height={150} width={150} alt="loader" />
           </div>
-        </form>
+        )
       ) : (
         <div className="card">
           <h1>Copy It!</h1>
